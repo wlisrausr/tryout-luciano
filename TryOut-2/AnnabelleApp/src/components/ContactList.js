87c25f1635a5f05fdad7db936aa5971528206ce9
Wanda Ichsanul Isra <wandaisra@gmail.com>
2017-07-13 17:37:30 +0700

    Remove unnecessary code

--- TryOut-2/AnnabelleApp/src/components/ContactList.js
+++ TryOut-2/AnnabelleApp/src/components/ContactList.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { ScrollView } from 'react-native';
 import ContactDetail from './ContactDetail';
-// import Button from './Button';
 import CustomData from '../contacts.json';
 
 class ContactList extends Component {
